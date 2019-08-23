fc162e7616c15cea2f5ffab48883ae9750ef565b
gwyche <gregory.wyche@protonmail.com>
2019-08-23 12:20:56 -0400

    Part 1 of JS homework, Angular HW 1, 2, and Hero App

---
+++ AngularHW1and2andHero/hw2/src/app/newcomponent2/newcomponent2.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {person} from '../person';
+import {PersonService} from '../person.service';
+import { peoplelist } from './peoplelist';
+import { PeoplelistService } from '../peoplelist.service';
+
+
+
+@Component({
+  selector: 'app-newcomponent2',
+  templateUrl: './newcomponent2.component.html',
+  styleUrls: ['./newcomponent2.component.css']
+})
+
+export class Newcomponent2Component implements OnInit {
+
+  guy: person;
+  list: peoplelist; 
+ 
+
+  //Inject a PersonService named humanservice
+  constructor(private humanservice: PersonService, private peoplelistservice: PeoplelistService) {}
+
+  getHuman(): void{
+    this.guy = this.humanservice.getDefaultPerson();
+  }
+
+  getList(): void{
+    this.list = this.peoplelistservice.list;
+  }
+
+  ngOnInit(){
+     this.getHuman();
+     this.getList();
+  }
+
+
+
+}
